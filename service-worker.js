d6653447027e66fde7c62e12805e278de9e6024c
marcosDioni <marcosptga22@gmail.com>
2025-07-08 11:21:31 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'bebeemdia-cache-v1'; // Aumente esse número quando mudar algo
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  // outros arquivos estáticos, como CSS ou imagens, se houver
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
