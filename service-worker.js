fad3892b50bd9e2fe1ada4c17f37a3e4362c1a5c
marcosDioni <marcosptga22@gmail.com>
2025-07-08 11:53:12 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,11 @@
-const CACHE_NAME = 'bebeemdia-cache-v2';
+const CACHE_NAME = 'bebeemdia-cache-v3';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/service-worker.js'
+  '/manifest.json',
+  '/service-worker.js',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -29,7 +32,8 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
